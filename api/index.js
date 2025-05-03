744c005e4074f48180388ace56c8fa37b6a40ab3
pratham-jaiswal <prathamj0502@gmail.com>
2025-05-04 00:27:04 +0530

    Add connect with patreon, disconnect patreon, fetch user details, dynamic rate limits

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import { ChatOpenAI } from "@langchain/openai";
 import { ChatCohere } from "@langchain/cohere";
 import { rateLimit } from "express-rate-limit";
 import RedisStore from "rate-limit-redis";
+import querystring from "querystring";
 import * as cheerio from "cheerio";
 import validator from "validator";
 import "@dotenvx/dotenvx/config";
@@ -18,109 +19,6 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hi");
-});
-
-app.get("/favicon.ico", (req, res) => {
-  res.status(204).end();
-});
-
-app.get("/favicon.png", (req, res) => {
-  res.status(204).end();
-});
-
-const redisClient = new Redis(process.env.UPSTASH_URL, {
-  tls: {},
-  reconnectOnError: () => true,
-});
-
-redisClient.on("error", (err) => {
-  console.error("Redis error:", err);
-});
-
-app.use(
-  clerkMiddleware({
-    authorizedParties: [process.env.ALLOWED_ORIGIN],
-    secretKey: process.env.CLERK_SECRET_KEY,
-  })
-);
-app.use(helmet());
-
-app.get("/health", (req, res) => {
-  return res.json({ status: "ok" });
-});
-
-const corsOptions = {
-  origin: `${process.env.ALLOWED_ORIGIN}`,
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-function verifyWebhook(req, res, next) {
-  const payload = req.body;
-  const headers = req.headers;
-  const wh = new Webhook(process.env.SVIX_SECRET);
-
-  try {
-    let msg = wh.verify(JSON.stringify(payload), headers);
-  } catch (err) {
-    return res.status(400).json({});
-  }
-
-  next();
-}
-
-const userRateLimiter = rateLimit({
-  store: new RedisStore({
-    sendCommand: (...args) => redisClient.call(...args),
-  }),
-  windowMs: 6 * 60 * 60 * 1000,
-  max: 2,
-  keyGenerator: (req) => {
-    const cookieStr = req.headers.cookie;
-    if (cookieStr) {
-      const cookies = Object.fromEntries(
-        cookieStr.split(";").map((cookie) => {
-          const [key, value] = cookie.trim().split("=");
-          return [key, value];
-        })
-      );
-
-      const token = cookies["__session"];
-      if (token) {
-        try {
-          const base64Url = token.split(".")[1];
-          const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-          const jsonPayload = decodeURIComponent(
-            atob(base64)
-              .split("")
-              .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
-              .join("")
-          );
-          const payload = JSON.parse(jsonPayload);
-          const userId = payload.userId;
-          if (userId) return userId;
-        } catch (e) {
-          console.error("JWT decode error:", e);
-        }
-      }
-    }
-
-    return req.ip;
-  },
-  handler: (req, res) => {
-    res
-      .status(429)
-      .json({ error: "Too many requests. Please try again later." });
-  },
-});
-
-app.use("/validate-user", verifyWebhook);
-app.use("/tldr/url", ClerkExpressRequireAuth());
-app.use("/tldr/text", [ClerkExpressRequireAuth(), userRateLimiter]);
 
 mongoose
   .connect(`${process.env.MONGODB_URI}`, { family: 4 })
@@ -157,12 +55,183 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  patreonDetails: {
+    patreonId: {
+      type: String,
+      required: true,
+    },
+    tier: {
+      type: String,
+      required: true,
+    },
+  },
   createdAt: {
     type: Date,
   },
 });
 
 const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).end();
+});
+
+app.get("/favicon.png", (req, res) => {
+  res.status(204).end();
+});
+
+const redisClient = new Redis(process.env.UPSTASH_URL, {
+  tls: {},
+  reconnectOnError: () => true,
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+app.use(
+  clerkMiddleware({
+    authorizedParties: [
+      process.env.ALLOWED_ORIGIN,
+      new URL(process.env.PATREON_REDIRECT_URI).origin,
+    ],
+    secretKey: process.env.CLERK_SECRET_KEY,
+  })
+);
+app.use(helmet());
+
+app.get("/health", (req, res) => {
+  return res.json({ status: "ok" });
+});
+
+const corsOptions = {
+  origin: `${process.env.ALLOWED_ORIGIN}`,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+function verifyWebhook(req, res, next) {
+  const payload = req.body;
+  const headers = req.headers;
+  const wh = new Webhook(process.env.SVIX_SECRET);
+
+  try {
+    let msg = wh.verify(JSON.stringify(payload), headers);
+  } catch (err) {
+    return res.status(400).json({});
+  }
+
+  next();
+}
+
+function extractUserIdFromToken(req, res, next) {
+  try {
+    if (!req.headers) {
+      return next();
+    }
+
+    const cookieStr = req.headers.cookie;
+    if (!cookieStr) {
+      req.headers["tldr-ai-clerk-user-id"] = null;
+      return next();
+    }
+    const cookies = Object.fromEntries(
+      cookieStr.split(";").map((cookie) => {
+        const [key, value] = cookie.trim().split("=");
+        return [key, value];
+      })
+    );
+    const token = cookies?.["__session"];
+    if (!token) {
+      req.headers["tldr-ai-clerk-user-id"] = null;
+      return next();
+    }
+
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+        .join("")
+    );
+    const payload = JSON.parse(jsonPayload);
+    const userId = payload.userId;
+    req.headers["tldr-ai-clerk-user-id"] = userId;
+
+    return next();
+  } catch (error) {
+    req.headers["tldr-ai-clerk-user-id"] = null;
+    return next();
+  }
+}
+
+app.use(extractUserIdFromToken);
+
+const rateLimits = {
+  free: 1,
+  supporter: 3,
+  "digital insider": 10,
+};
+
+async function applyDynamicRateLimit(req, res, next) {
+  if (!req.headers) {
+    return next();
+  }
+
+  const userId = req.headers?.["tldr-ai-clerk-user-id"];
+  if (!userId) {
+    req.headers["tldr-ai-clerk-max-rate-limit"] = "1";
+    return next();
+  }
+
+  try {
+    const user = await User.findOne({ userId });
+
+    const tier = user?.patreonDetails?.tier?.toLowerCase();
+    const maxRequests = rateLimits[tier] || 1;
+
+    req.headers["tldr-ai-clerk-max-rate-limit"] = maxRequests.toString();
+  } catch (err) {
+    console.error("Rate limit tier lookup failed:", err);
+    req.headers["tldr-ai-clerk-max-rate-limit"] = "1";
+  }
+
+  next();
+}
+
+app.use(applyDynamicRateLimit);
+
+const userRateLimiter = rateLimit({
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+  windowMs: 6 * 60 * 60 * 1000,
+  max: (req) => {
+    const parsed = parseInt(req.headers["rate-limit-max"] || "1", 10);
+    return isNaN(parsed) ? 1 : parsed;
+  },
+  keyGenerator: (req) => {
+    if (req.headers?.["tldr-ai-clerk-user-id"]) {
+      return req.headers["tldr-ai-clerk-user-id"];
+    }
+    return req.ip;
+  },
+  handler: (req, res) => {
+    res
+      .status(429)
+      .json({ error: "Too many requests. Please try again later." });
+  },
+});
+
+app.use("/validate-user", verifyWebhook);
+app.use("/tldr/url", ClerkExpressRequireAuth());
+app.use("/tldr/text", [ClerkExpressRequireAuth(), userRateLimiter]);
 
 function fetchWebpageContent(url) {
   return axios
@@ -252,6 +321,7 @@ async function summarizeWebpage(url, model, content) {
     return null;
   }
 }
+
 const validModels = ["gpt-4o-mini", "gpt-4.1-nano"];
 
 app.post("/validate-user", async (req, res) => {
@@ -278,6 +348,7 @@ app.post("/validate-user", async (req, res) => {
         email: email,
         userId: id,
         createdAt: created_at,
+        patreonDetails: {},
       });
       await newUser.save();
     }
@@ -370,6 +441,111 @@ app.get("/fetch-saved-tldrs", async (req, res) => {
   } catch (error) {
     console.error("Error fetching saved TL;DRs:", error);
     return res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+app.get("/auth/patreon", (req, res) => {
+  const url = `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${
+    process.env.PATREON_CLIENT_ID
+  }&redirect_uri=${encodeURIComponent(
+    process.env.PATREON_REDIRECT_URI
+  )}&scope=identity%20identity.memberships`;
+
+  return res.redirect(url);
+});
+
+app.get("/connect/patreon/callback", async (req, res) => {
+  const code = req.query.code;
+
+  try {
+    const tokenRes = await axios.post(
+      "https://www.patreon.com/api/oauth2/token",
+      querystring.stringify({
+        code,
+        grant_type: "authorization_code",
+        client_id: process.env.PATREON_CLIENT_ID,
+        client_secret: process.env.PATREON_CLIENT_SECRET,
+        redirect_uri: process.env.PATREON_REDIRECT_URI,
+      }),
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    );
+
+    const accessToken = tokenRes.data.access_token;
+
+    const identityRes = await axios.get(
+      "https://www.patreon.com/api/oauth2/v2/identity?include=memberships&fields[user]=email,first_name,full_name,image_url,last_name,social_connections,thumb_url,url,vanity",
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+
+    const member_id = identityRes?.data?.included?.[0]?.id;
+
+    if (member_id) {
+      const memberUrl = `https://www.patreon.com/api/oauth2/v2/members/${member_id}?include=currently_entitled_tiers&fields%5Btier%5D=title`;
+      const response = await axios.get(memberUrl, {
+        headers: {
+          Authorization: `Bearer ${process.env.PATREON_CREATOR_ACCESS_TOKEN}`,
+        },
+      });
+
+      const tier = response?.data?.included?.[0]?.attributes?.title;
+      await User.findOneAndUpdate(
+        { userId: req.headers["tldr-ai-clerk-user-id"] },
+        { $set: { patreonDetails: { patreonId: member_id, tier } } },
+        { upsert: true, new: true }
+      );
+    }
+
+    return res.redirect("http://192.168.29.237:3000");
+  } catch (err) {
+    console.error("Patreon OAuth error:", err.response?.data || err.message);
+    return res.status(500).send("OAuth failed");
+  }
+});
+
+app.get("/get-user", async (req, res) => {
+  try {
+    const existingUser = await User.findOne({
+      userId: req.headers["tldr-ai-clerk-user-id"],
+    });
+
+    if (!existingUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const tier = existingUser.patreonDetails?.tier?.toLowerCase();
+    if (!tier || !rateLimits[tier]) {
+      return res.status(400).json({ error: "Invalid or missing tier" });
+    }
+
+    const userDetails = {
+      ...existingUser.toObject(),
+      patreonDetails: {
+        ...existingUser.patreonDetails,
+        rateLimit: rateLimits[tier],
+      },
+    };
+
+    return res.json({ userDetails: userDetails });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/diconnect-patreon", async (req, res) => {
+  try {
+    const result = await User.findOneAndUpdate(
+      { userId: req.headers["tldr-ai-clerk-user-id"] },
+      { $set: { patreonDetails: {} } },
+      { upsert: true, new: true }
+    );
+
+    return res.json({ userDetails: result });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
